bc6d365811c891e0cb82a5a3709b379efab82a50
randyrandy91 <randytheman01@yahoo.com>
2017-04-30 21:52:51 -0500

    Cleaned up code created clear function.

--- the-odin-project/calculator/js/scripts.js
+++ the-odin-project/calculator/js/scripts.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
-    var numbers = [0];
-    var pushedNum = "";
-    var result = 0;
-    var operatorList = [];
-    var lastNum = 0;
-    var lastOperator;
-    var count = 0;
 
+    var numbers = [0],
+         pushedNum = "",
+         result = 0,
+         operatorList = [],
+         lastNum = 0,
+         lastOperator,
+         isExpression = false;
+
+    console.log("not the init.");
+
+    // Displays clicked numbers in display area.
     $("body").on("click", ".num", function(){
         pushedNum += $(this).text();
         $(".display").text(pushedNum);
@@ -14,112 +18,99 @@ $(document).ready(function(){
 
     $(".operator").on("click", function() {
 
+        /* Cannot make pushedNum 0 as that would display a 0 in the display area.
+        This prevents calculator from breaking if an operator is clicked before any operands are clicked.
+        */
         if (pushedNum == "") {
             pushedNum = 0;
         }
-            operatorList.push($(this).text());
-            numbers.push(pushedNum);
-            lastNum = numbers.slice(-1);
-            lastOperator = operatorList.slice(-2)[0];
 
-            console.log("lastNum is " + lastNum);
-            console.log("lastOperator is " + lastOperator);
-            console.log("numbers is " + numbers);
-            console.log("operatorList is " + operatorList);
+        operatorList.push($(this).text());
+        numbers.push(pushedNum);
+        lastNum = numbers.slice(-1);
+        lastOperator = operatorList.slice(-2)[0];
 
-            console.log(parseFloat(result) + " ... " + parseFloat(lastNum));
+        switch (lastOperator) {
+            case "+":
+                // Forces calculation to wait until two operands are clicked. Otherwise, the operator would run on one operand which would provide incorrect results.
+                isExpression ? result = add(parseFloat(result), parseFloat(lastNum)) : result = pushedNum;
+                break;
+            case "-":
+                isExpression ? result = subtract(parseFloat(result), parseFloat(lastNum)) : result = pushedNum;
+                break;
+            case "*":
+                isExpression ? result = multiply(parseFloat(result), parseFloat(lastNum)) : result = pushedNum;
+                break;
+            case "/":
+                isExpression ? result = divide(parseFloat(result), parseFloat(lastNum)) : result = pushedNum;
+                break;
+        }
 
+        $(".display").text(Math.round(parseFloat(result) * 100000) / 100000); // Round to nearest .00000th place
 
-            switch (lastOperator) {
-                case "+":
-                    result = add(parseFloat(result), parseFloat(lastNum));
-                    break;
-                case "-":
-                    if (count > 0) {
-                        result = subtract(parseFloat(result), parseFloat(lastNum));
-                    } else {
-                        result = pushedNum;
-                    }
-                    break;
-                case "*":
-                    if (count > 0) {
-                        result = multiply(parseFloat(result), parseFloat(lastNum));
-                    } else {
-                        result = pushedNum;
-                    }
-                    break;
-                case "/":
-                    if (count > 0) {
-                        result = divide(parseFloat(result), parseFloat(lastNum));
-                    } else {
-                        result = pushedNum;
-                    }
-                    break;
-            }
-
-            console.log("result is " + result);
-
-            console.log("");
-            if (count > 0) {
-                $(".display").text(result);
-            };
-
-            count++;
-
+        isExpression = true;
         pushedNum = "";
     });
 
     $("#equal").on("click", function() {
         numbers.push(pushedNum);
         lastNum = $(".display").text();
-        lastOperator = operatorList.slice(-1)[0];
+        lastOperator = operatorList.slice(-1)[0]; // Pull last operator in the operatorList array
 
-        switch (lastOperator) {
-            case "+":
-                if (count > 0) {
-                    result = add(parseFloat(result), parseFloat(lastNum));
-                    console.log("it was a plus!!");
-                }
-                break;
-            case "-":
-                if (count > 0) {
-                    result = subtract(parseFloat(result), parseFloat(lastNum));
-                }
-                break;
-            case "*":
-                result = multiply(parseFloat(result), parseFloat(lastNum));
-                break;
-            case "/":
-                result = divide(parseFloat(result), parseFloat(lastNum));
-                break;
+        if (lastOperator) {
+            switch (lastOperator) {
+                case "+":
+                    // Forces calculation to wait until two operands are clicked. Otherwise, the operator would run on one operand which would provide incorrect results.
+                    isExpression ? result = add(parseFloat(result), parseFloat(lastNum)) : result = pushedNum;
+                    break;
+                case "-":
+                    // Forces calculation to wait until two operands are clicked. Otherwise, the operator would run on one operand which would provide incorrect results.
+                    isExpression ? result = subtract(parseFloat(result), parseFloat(lastNum)) : result = pushedNum;
+                    break;
+                case "*":
+                    // Forces calculation to wait until two operands are clicked. Otherwise, the operator would run on one operand which would provide incorrect results.
+                    isExpression ? result = multiply(parseFloat(result), parseFloat(lastNum)) : result = pushedNum;
+                    break;
+                case "/":
+                    // Forces calculation to wait until two operands are clicked. Otherwise, the operator would run on one operand which would provide incorrect results.
+                    isExpression ? result = divide(parseFloat(result), parseFloat(lastNum)) : result = pushedNum;
+                    break;
+                case "=":
+                    break;
+            }
+
+            $(".display").text(Math.round(parseFloat(result) * 100000) / 100000); // Round to nearest .00000th place
+            pushedNum = "";
         }
-
-        $(".display").text(result);
-        pushedNum = "";
     });
 
     $("#clear").on("click", function() {
+        clear();
+        console.log("result is " + result);
+    });
+
+    function add(a, b){
+        return a + b;
+    }
+    function subtract(a, b){
+        return a - b;
+    }
+    function multiply(a, b){
+        return a * b;
+    }
+    function divide(a, b){
+        return a / b;
+    }
+    function clear(){
         $(".display").text(0);
-        pushedNum = "";
         numbers = [0];
+        pushedNum = "";
         result = 0;
         operatorList = [];
-        count = 0;
-    });
-});
+        lastNum = 0;
+        lastOperator = "";
+        isExpression = false;
+        console.log("is this running?");
+    }
 
-function add(a, b){
-    return a + b;
-}
-function subtract(a, b){
-    return a - b;
-}
-function multiply(a, b){
-    return a * b;
-}
-function divide(a, b){
-    return a / b;
-}
-function equals(numbers){
-    console.log("..." + numbers);
-}
+});
